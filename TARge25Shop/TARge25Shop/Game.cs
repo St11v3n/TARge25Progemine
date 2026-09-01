eb5b0b6fc49ce095e37f7fc027879f7d4aebfa7f
Opilane Techno+TLN <241931390+St11v3n@users.noreply.github.com>
2026-09-01 11:44:13 +0300

    continuation of making the maze

--- TARge25Shop/TARge25Shop/Game.cs
+++ TARge25Shop/TARge25Shop/Game.cs
@@ -7,12 +7,87 @@ namespace TARge25Shop
 {
     class Game
     {
+        private World MyWorld;
+            private Player CurrentPlayer;
         public void Start()
         {
-            WriteLine("Game is starting");
+        
 
-            WriteLine("\n\nPress any key to exit....");
-            ReadKey(true);
+            string[,] grid = {
+                { "=", "=", "=", "=", "=", "=", "=" },
+                { "=", " ", "=", " ", " ", " ", "X" },
+                { " ", " ", "=", " ", "=", " ", "=" },
+                { "=", " ", "=", " ", "=", " ", "=" },
+                { "=", " ", " ", " ", "=", " ", "=" },
+                { "=", "=", "=", "=", "=", "=", "=" },
+            };
+            MyWorld = new World(grid);
+            
+
+            CurrentPlayer = new Player(0, 2);
+
+
+            RunGameLoop();
+            
+        }
+
+        private void DrawFrame()
+        {
+            Clear();
+            MyWorld.Draw();
+            CurrentPlayer.Draw();
+        }
+        private void HandlePLayerInput()
+        {
+            ConsoleKeyInfo keyInfo = ReadKey(true);
+            ConsoleKey key = keyInfo.Key;
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X, CurrentPlayer.Y - 1))
+                    {
+                        CurrentPlayer.Y -= 1;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X, CurrentPlayer.Y + 1))
+                    {
+                        CurrentPlayer.Y += 1;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X - 1, CurrentPlayer.Y ))
+                    {
+                        CurrentPlayer.X -= 1;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X + 1, CurrentPlayer.Y))
+                    {
+                        CurrentPlayer.X += 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void RunGameLoop()
+        {
+            while (true)
+            {
+                // Draw everything
+                DrawFrame();
+
+                // Check for player input from the keyboard and move the player
+                HandlePLayerInput();
+                // Check if the player has reached the exit and end the game if so
+                //TODO!
+                string elementAtPlayerPos = MyWorld.GetElementAt(CurrentPlayer.X, CurrentPlayer.Y);
+                MyWorld.Grid[]
+                //Give the Console a chance to render.
+                System.Threading.Thread.Sleep(20);
+            }
+
         }
     }
 }
--- TARge25Shop/TARge25Shop/Player.cs
+++ TARge25Shop/TARge25Shop/Player.cs
@@ -1,6 +1,30 @@
-﻿namespace TARge25Shop
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace TARge25Shop
 {
-    public class Player
+    class Player
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private string PlayerMarker;
+        private ConsoleColor PlayerColor;
+
+        public Player(int initialX, int initialY)
+        {
+            X = initialX;
+            Y = initialY;
+            PlayerMarker = "O";
+            PlayerColor = ConsoleColor.Red;
+        }
+        public void Draw()
+        {
+            ForegroundColor = PlayerColor;
+            SetCursorPosition(X, Y);
+            Write(PlayerMarker);
+            ResetColor();
+        }
     }
 }
