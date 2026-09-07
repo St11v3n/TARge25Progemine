71e10c29ae282e1c5ece34e85c2874e2690cc77e
KevinKuur <241931390+St11v3n@users.noreply.github.com>
2026-09-07 16:17:39 +0300

    classes

---
+++ TARge25Shop/TARge25Shop.ApplicationServices/Services/SpaceshipServices.cs
@@ -0,0 +1,18 @@
+﻿
+using TARge25Shop.Core.Dto;
+using TARge25Shop.Core.Domain;
+
+namespace TARge25Shop.ApplicationServices.Services
+{
+    public class SpaceshipServices
+    {
+
+        public async Task<Spaceship> Create(SpaceshipDto dto)
+        {
+
+            return null;
+        }
+    }
+
+  
+}
