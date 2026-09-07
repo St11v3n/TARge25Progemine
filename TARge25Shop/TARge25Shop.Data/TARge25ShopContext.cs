d4e900f3efb9e6a990d0e770722282c9174c3fcc
KevinKuur <241931390+St11v3n@users.noreply.github.com>
2026-09-07 15:28:58 +0300

    multiple projects added

---
+++ TARge25Shop/TARge25Shop.Data/TARge25ShopContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TARge25Shop.Data
+{
+    //nimetasime classi TARge25ShopContext, mis pärib DbContext klassi
+    public class TARge25ShopContext : DbContext
+    {
+        //see class tuleb teha DbContextiks,
+        //et saaks kasutada Entity Frameworki andmebaasi operatsioone
+        //tegime konteksti, mis pärib DbContext klassi
+        public TARge25ShopContext(DbContextOptions<TARge25ShopContext> options)
+            :base(options) { }
+
+
+    }
+
+   
+}
