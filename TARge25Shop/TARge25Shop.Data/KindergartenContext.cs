0eb47cbcc520f07281686dcbbcffca9f98cd841f
St11v3n <stiiven111@gmail.com>
2026-09-20 20:19:13 +0300

    Koostsin KindergartenContext klassi

---
+++ TARge25Shop/TARge25Shop.Data/KindergartenContext.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using TARge25Shop.Core.Domain;
+
+namespace TARge25Shop.Data
+{
+    //nimetasime classi TARge25ShopContext, mis pärib DbContext klassi
+    public class KindergartenContext : DbContext
+    {
+        //see class tuleb teha DbContextiks,
+        //et saaks kasutada Entity Frameworki andmebaasi operatsioone
+        //tegime konteksti, mis pärib DbContext klassi
+        public KindergartenContext(DbContextOptions<KindergartenContext> options)
+            : base(options) { }
+
+        //vaja lisada dbSet, mis on seotud meie domain klassiga Spaceship
+        public DbSet<Kindergarten> Kindergartens { get; set; }
+    }
+
+
+
+}
