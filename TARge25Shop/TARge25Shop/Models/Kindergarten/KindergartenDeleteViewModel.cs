5bc7252a97bbf43bdf27934a52fb32e8fc7ddd27
St11v3n <stiiven111@gmail.com>
2026-09-20 20:25:49 +0300

    Koostsin ViewModel-sid koostmise, kustutamise, detailide, indexi, uuenduse ning koostmise kui ka uuenduse jaoks Kindergarten alam kaustas Models kausta jaoks.

---
+++ TARge25Shop/TARge25Shop/Models/Kindergarten/KindergartenDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TARge25Shop.Models.Kindergarten
+{
+    public class KindergartenDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public string ChildrenCount { get; set; } = string.Empty;
+        public int KindergartenName { get; set; }
+        public int TeacherName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
