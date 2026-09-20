1789fd466bf1d566d7edee2000ae95da37554888
St11v3n <stiiven111@gmail.com>
2026-09-20 20:17:30 +0300

    Koostasin Kindergarten klassi domainile

---
+++ TARge25Shop/TARge25Shop.Core/Domain/Kindergarten.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TARge25Shop.Core.Domain
+{
+    public class Kindergarten
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public string ChildrenCount { get; set; } = string.Empty;
+        public int KindergartenName { get; set; }
+        public int TeacherName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
