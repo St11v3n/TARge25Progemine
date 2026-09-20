bf9e93deee85944eb1a6ebdc97a6378d8f6d771f
St11v3n <stiiven111@gmail.com>
2026-09-20 20:23:09 +0300

    Koostsin IKindergartenServices klassi ServiceInterface kasutas

---
+++ TARge25Shop/TARge25Shop.Core/ServiceInterface/IKindergartenServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TARge25Shop.Core.Domain;
+using TARge25Shop.Core.Dto;
+
+namespace TARge25Shop.Core.ServiceInterface
+{
+    public interface IKindergartenServices
+    {
+        Task<Kindergarten> Create(KindergartenDto dto);
+        Task<Kindergarten> Update(KindergartenDto dto);
+
+        Task<Kindergarten> DetailAsync(Guid id);
+
+        Task<Kindergarten> Delete(Guid id);
+    }
+}
