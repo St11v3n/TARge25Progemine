e3ef33ee22ddc123e68a74286fa99fe4ada76c09
St11v3n <stiiven111@gmail.com>
2026-09-20 20:19:53 +0300

    Koostsin KindergartenDto klassi Dto kaustas

---
+++ TARge25Shop/TARge25Shop.Core/Dto/KindergartenDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TARge25Shop.Core.Dto
+{
+    public class KindergartenDto
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public string ChildrenCount { get; set; } = string.Empty;
+        public int KindergartenName { get; set; }
+        public int TeacherName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
